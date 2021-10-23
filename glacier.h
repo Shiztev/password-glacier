6a5be9c46017b077e625edefadf421817d4f8864
8xKetch <stevie.6strings@gmail.com>
2021-10-22 20:40:14 -0400

    added initial password functional interface

--- glacier.h
+++ glacier.h
@@ -1,13 +1,26 @@
 /**
  * @file glacier.h
- * @brief
+ * @brief Definitions for password generating functions.
  * 
  * @author Stevie Alvarez
  */
 
+
 #ifndef GLACIER_H
 #define GLACIER_H
 
-// stuff and things go here
+
+/**
+ * Generates a randomized password of length l consisting of num 
+ * integers, sp special characters, and (l - num - sp) alphabetic 
+ * characters.
+ * 
+ * @param l desired length of password
+ * @param num number of integers to be in password
+ * @param sp number of special characters to be in password
+ * @return a randomized password, within the provided parameters
+ */
+char * password(int l, int num, int sp);
+
 
 #endif
