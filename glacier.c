6000667166aeab5069effcf3102f9edb1b38a0ba
8xKetch <stevie.6strings@gmail.com>
2021-10-22 21:31:46 -0400

    added macros and constants, will attempt to reduce number of constants later

--- glacier.c
+++ glacier.c
@@ -1,6 +1,6 @@
 /**
  * @file glacier.c
- * @brief
+ * @brief Generates passwords built around user specifications.
  * 
  * Compile: gcc -Wall -Wextra -pedantic -ggdb -std=c99 -c glacier.c
  * @author Stevie Alvarez
@@ -12,6 +12,47 @@
 #include "glacier.h"
 
 
+/* Initial implementation with hardcoded options. Look for ways to expand
+    to a more calculating way of generating characters (could use ascii, 
+    but special characters get weird). */
+
+
+/**
+ * Max number of characters constituting password.
+ */
+#define MAX_LEN 128
+
+
+/**
+ * Minimum number of characters constituting password.
+ */
+#define MIN_LEN 4
+
+
+/**
+ * Array of possible numeric characters.
+ */
+const char * NUMBERS = "1234567890";
+
+
+/**
+ * Array of possible special characters.
+ */
+const char * SPECIALS = "!@#$%^&*";
+
+
+/**
+ * Array of possible upper-case alphabetic characters.
+ */
+const char * UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+/**
+ * Array of possible lower-case alphabetic characters.
+ */
+const char * LOWER = "abcdefghijklmnopqrstuvwxyz";
+
+
 /**
  * Generates a password with respect to parameters.
  */
@@ -19,9 +60,18 @@ char * password(int l, int num, int sp) {
     // declare (and initialize) variables
     char pswrd[l];
 
+    // check if l within max/min length
+
     // check params are valid (l >= (num + sp))
 
-    // do some stuff to pswrd
+    // choose num numeric characters
+
+    // choose sp special characters
+
+    // choose l - num - sp alphabetic characters
+
+    // randomly insert characters into string such that
+    // they are not inserted on a used index
 
     return pswrd;
 }
