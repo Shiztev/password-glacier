ec30e472390a46ae05a69c2e1ce4ee8a61aca6b3
8xKetch <stevie.6strings@gmail.com>
2021-10-22 20:40:39 -0400

    stubbed password, added comment outline to main

--- glacier.c
+++ glacier.c
@@ -9,15 +9,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "glacier.h"
 
 
-/* Body goes here */
+/**
+ * Generates a password with respect to parameters.
+ */
+char * password(int l, int num, int sp) {
+    // declare (and initialize) variables
+    char pswrd[l];
+
+    // check params are valid (l >= (num + sp))
+
+    // do some stuff to pswrd
+
+    return pswrd;
+}
 
 
 
 /**
- * Main
+ * Allows the user to specify how they would like their 
+ * password formated.
+ * 
+ * @param argc number of command line arguments passed in
+ * @param argv string array of command line arguments
+ * @return 0 if successful, 1 otherwise
  */
-int main(void) {
+int main(int argc, char * argv[]) {
+    // if user provides args from command line, utilize those 
+    // to generate a password, print the password, then exit.
 
+    // otherwise, loop, gathering user input, and produce 
+    // passwords w/r/t user input. Exit on EOF or dedicated 
+    // quit statement.
+
+
+    return 0;
 }
