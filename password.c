d3905d15fd058b0a4daf8ac4f4f5e7a6b49ae580
8xKetch <stevie.6strings@gmail.com>
2022-01-05 22:30:54 -0500

    redesigning so that char types are randomly selected with accordance with parameters

--- password.c
+++ password.c
@@ -15,21 +15,15 @@
 
 
 /**
- * Minimum number of characters constituting password.
+ * Maximum number of possible characters that can be used in a password.
  */
-#define MIN_LEN 3
+#define MAX_CHARS 68
 
 
 /**
- * Length of SPECIALS.
+ * Array of possible capital alphabetical characters.
  */
-#define SP_LEN 7
-
-
-/**
- * Array of possible special characters.
- */
-const char * SPECIALS = "!@#$%^&*";
+static const char * CHARS = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789!@#$%^&*";
 
 
 /**
@@ -42,19 +36,19 @@ const char * SPECIALS = "!@#$%^&*";
  */
 static char * make_password(int l, int num, int sp) {
     // declare (and initialize) variables
-    int * indexs = (int *)malloc(l * sizeof(char)); // dynamically alloced indexs
     int c = (l - num) - sp;  // number of alphabetic characters to be in password
     char pswrd[l];  // password's character buffer
-    char ints[num];  // numerical character buffer
-    char special[sp];  // special character buffer
-    char alpha[c];  // alphabetical character buffer
+
+    // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    // find API for sets for type data structure?
+    /**
+     * Data structure containing the current possible data types that can be
+     * appended to the password. 
+     */
+    int type [] = {0, 1, 2, 3}
 
     srand(time(NULL));  // set random seed, is more secure way to do this? yes: openssl rand/RAND_bytes
     // https://stackoverflow.com/questions/822323/how-to-generate-a-random-int-in-c/39475626#39475626
-
-    for (int i = 0; i < l; i++) {  // find api for a set implementation
-        indexs[i] = i;
-    }
 
     // check if l within max/min length
     if (!((l <= MAX_LEN) && (l >= MIN_LEN))) {
@@ -67,29 +61,10 @@ static char * make_password(int l, int num, int sp) {
         return NULL;
     }
 
-    // choose num numeric characters
-    for (int i = 0; i < num; i++) {
-        ints[i] = rand() % 10;
-    }
+    // randomly select chars from possible chars and insert them to password
+    for (int i = 0; i < l; ++i) {
 
-    // choose sp special characters
-    for (int i = 0; i < sp; i++){
-        special[i] = SPECIALS[rand() % SP_LEN];
     }
-
-    // choose l - num - sp alphabetic characters
-    for (int i = 0; i < c; i++) {
-        // randomly determine if it should be upper-case/lower-case
-        if ((rand() % 2) == 0) {  // upper-case
-            alpha[i] = (char)(65 + (rand() % 26));  // ascii translation
-        
-        } else {  // lower-case
-            alpha[i] = (char)(97 + (rand() % 26));  // ascii translation
-        }
-    }
-
-    // randomly insert characters into string such that
-    // they are not inserted on a used index
 
     // maybe add feature so that params are passed by reference, reassigned
     // values (e.g. NULL), and deallocated for security?
