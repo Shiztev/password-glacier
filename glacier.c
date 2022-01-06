c55716f53c53fe6b27914cea86cef8dffe618475
8xKetch <stevie.6strings@gmail.com>
2022-01-05 21:39:34 -0500

    implemented basic structure for standard input, began implementing repitition conditionals for standard input

--- glacier.c
+++ glacier.c
@@ -54,6 +54,13 @@ char ** PROMPTS = {"Passowrd Length: ", "Number of Integers: ", "Number of Speci
 
 
 /**
+ * Lengths of password elements.
+ *      Order: PASSWORD LENGTH, NUMBER OF INTS, NUMBER OF SPECIAL CHARS
+ */
+int LENGTHS[] = {DEF_LEN, DEF_INTS, DEF_SP};
+
+
+/**
  * @brief Allows the user to specify how they would like their 
  * password formated.
  * 
@@ -62,31 +69,59 @@ char ** PROMPTS = {"Passowrd Length: ", "Number of Integers: ", "Number of Speci
  * @return 0 if successful, 1 otherwise
  */
 int main(int argc, char * argv[]) {
-    /**
-     * Lengths of password elements.
-     *      Order: PASSWORD LENGTH, NUMBER OF INTS, NUMBER OF SPECIAL CHARS
-     */
-    int lengths[] = {DEF_LEN, DEF_INTS, DEF_SP};
+    /// Temporary storage for input.
+    char* tmp;
 
     // ----- CLI INPUT --------------------------------------------------------
-    for (int i = 1; i < argc; ++i) {
-        lengths[i - 1] = strtol(argv[i], NULL, 10);
-    }
+    if (argc > 1) {
 
-    // to generate a password, print the password, then exit.
-    printf("%s\n", password(lengths[0], lengths[1], lengths[2]));
+        for (int i = 1; i < argc; ++i) {
+            LENGTHS[i - 1] = strtol(argv[i], NULL, 10);
+        }
+
+        // to generate a password, print the password, then exit.
+        printf("%s\n", password(LENGTHS[0], LENGTHS[1], LENGTHS[2]));
+    }
     // ------------------------------------------------------------------------
+    
 
     // otherwise, loop, gathering user input, and produce 
     // passwords w/r/t user input. Exit on EOF or dedicated 
     // quit statement.
     // ----- STD INPUT---------------------------------------------------------
-    while(1) {  // FINISH CONDITIONAL
+    printf(); // FIXME: prompt/instructions on input requirements
+    printf("Length of password cannot exceed %d.", MAX_LEN);  // FIXME: should be able to access MAX_LEN from password.c
+
+
+    do {
         for (int i = 0; i < PARTS; ++i) {
             printf("%s\n", PROMPTS[i]);
+            scanf("%s", tmp);
+            if (!strcmp(tmp, "\0")) {  // if input isn't empty (EOF/New line? just hits enter)
+                LENGTHS[i] = (int)strtol(tmp, NULL, 10);
+            }
+        }
+
+        // determine if the user would like another passowrd
+        printf("\n%s\n\nWould you like another password?", password(LENGTHS[0], LENGTHS[1], LENGTHS[2]));
+        scanf("%s", tmp);
+        
+        printf("\nDo you want to use the same settings as last time?\n");
+        if (!strcmp(tmp, "yes")) {  // check for other forms of 'yes'
+            
+                if (!strcmp(tmp, "yes")) {
+
+                } else if (!strcmp(tmp, "no")) {
+                    // reset default values
+                    LENGTHS[0] = DEF_LEN;
+                    LENGTHS[1] = DEF_INTS;
+                    LENGTHS[2] = DEF_SP;
+                }
+
 
         }
-    }
+
+    } while(1);
 
 
     return 0;
