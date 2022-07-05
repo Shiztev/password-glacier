1f5b1cde054204672b04b23e453c25f77d331e31
Stevie Alvarez <stevie.6strings@gmail.com>
2022-07-05 14:42:06 -0700

    make_password implementation that generates a random pword of length l

--- password.c
+++ password.c
@@ -58,6 +58,24 @@ static const int * STARTS = {0, 26, 52, 62, 71};
  */
 int type [] = {0, 1, 2};
 
+/**
+ * @brief Generate a password of length l.
+ * 
+ * @param l desired length of password
+ * @return a randomized password, constituted of numbers, letters, and special chars
+ */
+static char * make_password(int l) {
+  char * pswrd = malloc(l * sizeof(char));
+  srand(time(NULL));  // set random seed, is more secure way to do this? yes: openssl rand/RAND_bytes
+  // https://stackoverflow.com/questions/822323/how-to-generate-a-random-int-in-c/39475626#39475626
+
+  for (int i = 0; i < l; ++i) {
+    pswrd[i] = 33 + (rand() % 90);
+  }
+
+  return pswrd;
+}
+
 
 /**
  * @brief Generates a password with respect to parameters.
