ee9fa826e23929bc973807c58983cb19a1f9b23c
Stevie Alvarez <stevie.6strings@gmail.com>
2022-07-05 15:03:46 -0700

    update cli to fit changes to password generator

--- glacier.c
+++ glacier.c
@@ -30,56 +30,6 @@
 
 
 /**
- * Default number of special characters.
- */
-#define DEF_SP 4
-
-
-/**
- * Default number of integers.
- */
-#define DEF_INTS 4
-
-
-/**
- * Number of 'parts' of the password that can be customized. 
- */
-#define PARTS 3
-
-
-/**
- * List of prompts for user input, with respect to a 'part' of the password.
- */
-static const char ** PROMPTS = {"Passowrd Length: ", "Number of Integers: ", "Number of Special Characters: "};
-
-
-/**
- * Defualt lengths of password elements.
- *      Order: PASSWORD LENGTH, NUMBER OF INTS, NUMBER OF SPECIAL CHARS
- */
-static const int DEFAULTS[] = {DEF_LEN, DEF_INTS, DEF_SP};
-
-
-/**
- * @brief Gets lengths for password elements from standard input.
- */
-static void get_lengths(int * lengths) {
-    char * tmp;
-    for (int i = 0; i < PARTS; ++i) {
-        printf("%s\n", PROMPTS[i]);
-        scanf("%s", tmp);
-
-        if (!strcmp(tmp, "\0")) {  // if input isn't empty (EOF/New line? just hits enter)
-            lengths[i] = (int)strtol(tmp, NULL, 10);
-
-        } else {
-            lengths[i] = DEFAULTS[i];
-        }
-    }
-}
-
-
-/**
  * @brief Allows the user to specify how they would like their 
  * password formated.
  * 
@@ -88,82 +38,63 @@ static void get_lengths(int * lengths) {
  * @return 0 if successful, 1 otherwise
  */
 int main(int argc, char * argv[]) {
-    /// Length of lengths array
-    int l_len = 3;
-    /**
-     * Lengths of password elements.
-     *      Order: PASSWORD LENGTH, NUMBER OF INTS, NUMBER OF SPECIAL CHARS
-     */
-    int lengths[] = (int *)malloc(l_len * sizeof(int));
-    lengths[0] = DEF_LEN;
-    lengths[1] = DEF_INTS;
-    lengths[2] = DEF_SP;
+  /// Temporary storage for input.
+  char* tmp;
 
-    /// Temporary storage for input.
-    char* tmp;
+  /// Password storage.
+  char* pswrd;
 
-    /// Boolean determining if previous settings should be reused.
-    int rep_set = 0;
+  /// Boolean determining if previous settings should be reused.
+  int rep_set = 0;
 
-    // ------------------------------------------------------------------------
-    //    CLI INPUT
-    // ------------------------------------------------------------------------
-    if (argc > 1) {
+  /// Length of the password.
+  long length = DEF_LEN;
 
-        for (int i = 1; i < argc; ++i) {
-            lengths[i - 1] = strtol(argv[i], NULL, 10);
-        }
+  // ------------------------------------------------------------------------
+  //    CLI INPUT
+  // ------------------------------------------------------------------------
+  if (argc == 2) {
+    length = strtol(argv[1], NULL, 10);
+    pswrd = password(length);
+    printf("%s\n", pswrd);
 
-        // to generate a password, print the password, then exit.
-        printf("%s\n", password(lengths[0], lengths[1], lengths[2]));
-    }
-    // ------------------------------------------------------------------------
-    // ------------------------------------------------------------------------
-    
-
-    // ------------------------------------------------------------------------
-    //    STD INPUT
-    // ------------------------------------------------------------------------
-    // otherwise, loop, gathering user input, and produce 
-    // passwords w/r/t user input. Exit on EOF or dedicated quit statement.
-    printf("Enter in character boundaries for each password you choose to generate..."); // FIXME: prompt/instructions on input requirements
-    printf("Cumulative length of password cannot exceed %d.\n", MAX_LEN);  // FIXME: should be able to access MAX_LEN from password.c
-
-    do {
-        if (!rep_set) {
-            get_lengths(lengths);
-        }
-
-        // determine if the user would like another passowrd
-        printf("\n%s\n\nWould you like another password?", password(lengths[0], lengths[1], lengths[2]));
-        scanf("%s", tmp);
-
-        if (!strcmp(tmp, "yes")) {  // check for other forms of 'yes'
-
-            printf("\nDo you want to use the same settings as last time?\n");
-            if (!strcmp(tmp, "yes")) {
-                
-                    if (!strcmp(tmp, "yes")) {
-                        rep_set = 1;
-
-                    } else if (!strcmp(tmp, "no")) {
-                        // reset default values
-                        rep_set = 0;
-                        lengths[0] = DEF_LEN;
-                        lengths[1] = DEF_INTS;
-                        lengths[2] = DEF_SP;
-                    }
-            }
-
-        } else if (!strcmp(tmp, "no")) {  // check for other forms of no/quit
-            break;
-        }
-
-    } while(1);
-
-    free(lengths);
-    // ------------------------------------------------------------------------
-    // ------------------------------------------------------------------------
+    free(pswrd);
+    pswrd = NULL;
 
     return 0;
+  }
+  // ------------------------------------------------------------------------
+  // ------------------------------------------------------------------------
+
+
+  // ------------------------------------------------------------------------
+  //    STD INPUT
+  // ------------------------------------------------------------------------
+  // otherwise, loop, gathering user input, and produce 
+  // passwords w/r/t user input. Exit on EOF or dedicated quit statement.
+  printf("Enter in character boundaries for each password you choose to generate..."); // FIXME: prompt/instructions on input requirements
+  printf("Cumulative length of password cannot exceed %d.\n", MAX_LEN);  // FIXME: should be able to access MAX_LEN from password.c
+
+  do {
+    printf("Enter length: ");
+    scanf("%ld", &length);
+    pswrd = password(length);
+    printf("\n%s\n", pswrd);
+    free(pswrd);
+    pswrd = NULL;
+
+    printf("\nWould you like another password?");
+    scanf("%s", tmp);
+
+    if (!strcmp(tmp, "yes")) {  // check for other forms of 'yes'
+      rep_set = 1;
+
+    } else {
+      rep_set = 0;
+    }
+  } while(rep_set);
+  // ------------------------------------------------------------------------
+  // ------------------------------------------------------------------------
+
+  return 0;
 }
